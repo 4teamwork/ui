c552de987fc09833956f5ffd8fe6e89da222e85d
Joel-Luca <joel.christinet@gmx.ch>
2025-08-06 14:49:09 +0200

    Add an example component to the index for testing

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,17 @@
   <v-container>
     <h1>Content</h1>
     <p><router-link :to="localePath({ name: 'serverside-collection' })"> Serverside Collection </router-link></p>
+    <SelectSwitcher v-model="active" :items="items" jump />
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      active: null,
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'],
+    }
+  },
+}
 </script>
