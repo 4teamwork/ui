8e0bed6ba8352484c12250ca70a45068e80e3b87
Joel-Luca <joel.christinet@gmx.ch>
2025-09-04 14:22:22 +0200

    Add modal page with base modal example

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1>Content</h1>
     <p><router-link :to="localePath({ name: 'serverside-collection' })"> Serverside Collection </router-link></p>
     <p><router-link :to="localePath({ name: 'input' })"> Input fields </router-link></p>
+    <p><router-link :to="localePath({ name: 'modal' })"> Modals </router-link></p>
   </v-container>
 </template>
 
---
+++ pages/modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>Modal</h1>
+    <BaseModal :value="show" max-width="300">
+      <div class="pa-3">
+        <p class="pb-2">Base Modal</p>
+        <v-btn @click="show = false"> Close </v-btn>
+      </div>
+      <template #activator="props">
+        <v-btn v-bind="props" @click="show = true"> Open </v-btn>
+      </template>
+    </BaseModal>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+}
+</script>
