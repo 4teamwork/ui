f5c1c4c3540b45ef60b2f9f9e3af0d8a469fba8c
Joel-Luca <joel.christinet@gmx.ch>
2025-09-04 09:54:19 +0200

    Create own input page

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,10 @@
   <v-container>
     <h1>Content</h1>
     <p><router-link :to="localePath({ name: 'serverside-collection' })"> Serverside Collection </router-link></p>
-    <SelectSwitcher v-model="active" :items="items" jump />
+    <p><router-link :to="localePath({ name: 'input' })"> Input fields </router-link></p>
   </v-container>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      active: null,
-      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'],
-    }
-  },
-}
+export default {}
 </script>
---
+++ pages/input.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1>Input</h1>
+    <SelectSwitcher v-model="active" :items="items" jump />
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: null,
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'],
+    }
+  },
+}
+</script>
